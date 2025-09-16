6a43569c7339453c9c66e8240d8275e70a0fb7f2
AleccFilterPlusOÜ <alecc@filterplus.ee>
2025-09-16 16:32:08 +0200

    Add ordering and sequence fields to EstatePropertyTag and EstatePropertyType models, updating views accordingly for improved data organization and user interaction.

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -4,10 +4,10 @@ from odoo import models, fields
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
     _description = "Estate Property Type"
-    _order = "name"
+    _order = "sequence, name"
 
     name = fields.Char(string="Name", required=True)
-    
+    sequence = fields.Integer(string="Sequence", default=10)
 
     _sql_constraints = [
         ("estate_property_type_name_uniq", "unique(name)", "Property type name must be unique."),
