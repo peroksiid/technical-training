ed95dd255e2f09496967bde682dbdb515a2187e3
AleccFilterPlusOÜ <alecc@filterplus.ee>
2025-09-16 11:53:21 +0200

    Implement estate property views and update manifest to include new view file

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,14 +6,14 @@
     "data": [
         # Security files first
         "security/ir.model.access.csv",
-        
+
         # Data files in dependency order
         # "data/estate_property_type_data.xml",  # Base data first
         # "data/estate_property_tag_data.xml",   # Independent data
         # "data/estate_property_offer_data.xml", # Depends on property types
-        
+
         # Views and menus last
-        # "views/estate_property_views.xml",
+        "views/estate_property_views.xml",
         # "views/estate_property_type_views.xml",
         # "views/estate_property_tag_views.xml",
         # "views/estate_property_offer_views.xml",
