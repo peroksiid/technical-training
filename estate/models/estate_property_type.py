c842006e0eddd5f0985d4d7b57c7042308e61d95
AleccFilterPlusOÜ <alecc@filterplus.ee>
2025-09-16 16:10:50 +0200

    Add property relationship to EstatePropertyType model and update views to display related properties. Introduced a new notebook page for properties in the estate property type form for improved data organization and user interaction.

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -12,5 +12,12 @@ class EstatePropertyType(models.Model):
     _sql_constraints = [
         ("estate_property_type_name_uniq", "unique(name)", "Property type name must be unique."),
     ]
+    
+    # Inline list: related properties
+    property_ids = fields.One2many(
+        comodel_name="estate.property",
+        inverse_name="property_type_id",
+        string="Properties",
+    )
 
     
